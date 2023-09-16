29b6f2f6ea7f4eb1792b3d0e3b01fc4b9ec24148
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-09-16 00:57:44 -0700

    Update file names

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php 
-$prefix  = 'Thank you';
-$name    = 'Narendra';
-$message = "$prefix  $name";
+$item    = 'Chocolate';
+$stock   = 5;
+$wanted  = 8;
+$can_buy = ($wanted <= $stock);
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>String Operator</title>
+    <title>Comparison Operators</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2><?= $name ?>'s Order</h2>
-    <p><?= $message ?></p>
+    <h2>Shopping Cart</h2>
+    <p>Item:    <?= $item ?></p>
+    <p>Stock:   <?= $stock ?></p>
+    <p>Wanted:  <?= $wanted ?></p>
+    <p>Can buy: <?= $can_buy ?></p>
   </body>
 </html>
