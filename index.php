a4967c32fc2f451c066459a74a418015b1b8b5f6
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-09-16 01:09:44 -0700

    Updated names for files

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
-<?php 
-$item    = 'Chocolate';
-$stock   = 3;
-$wanted  = 5;
-$deliver = true;
-$can_buy = (($wanted <= $stock) && ($deliver == true));
+<?php
+$username = 'Ivy';                                   // Variable to hold username
+
+$greeting = 'Hello, ' . $username . '.';             // Greeting is 'Hello' + username
+
+$offer = [                                           // Create array to hold offer
+    'item'     => 'Chocolate',                       // Item on offer
+    'qty'      => 5,                                 // Quantity to buy
+    'price'    => 5,                                 // Usual price per pack
+    'discount' => 4,                                 // Offer price per pack
+];
+
+$usual_price = $offer['qty'] * $offer['price'];      // Usual total price
+$offer_price = $offer['qty'] * $offer['discount'];   // Offer total price
+$saving      = $usual_price - $offer_price;          // Total saving
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Logical Operators</title>
+    <title>The Candy Store</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h1>Shopping Cart</h1>
-    <p>Item:    <?= $item ?></p>
-    <p>Stock:   <?= $stock ?></p>
-    <p>Wanted: <?= $wanted ?></p>
-    <p>Can buy: <?= $can_buy ?></p>
+
+    <h2>Multi-buy Offer</h2>
+
+    <p><?= $greeting ?></p>
+
+    <p class="sticker">Save $<?= $saving ?></p>
+
+    <p>Buy <?= $offer['qty'] ?> packs of <?= $offer['item'] ?> 
+      for $<?= $offer_price ?><br> (usual price $<?= $usual_price ?>)</p> 
   </body>
 </html>
