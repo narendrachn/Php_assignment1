3bf2bc725415407b7a72608f277d433aa75ff2a3
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-09-16 00:44:41 -0700

    Change file name for convenience

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php 
-$items    = 12;
-$cost     = 7;
-$subtotal = $cost * $items;
-$tax      = ($subtotal / 100) * 20;
-$total    = $subtotal + $tax;
+$prefix  = 'Thank you';
+$name    = 'Ivy';
+$message = $prefix . ', ' . $name;
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Mathematical Operators</title>
+    <title>String Operator</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>Shopping Cart</h2>
-    <p>Items: <?= $items ?></p>
-    <p>Cost per pack: $<?= $cost ?></p>
-    <p>Subtotal: $<?= $subtotal ?></p>
-    <p>Tax: $<?= $tax ?></p>
-    <p>Total: $<?= $total ?></p>
+    <h2><?= $name ?>'s Order</h2>
+    <p><?= $message ?></p>
   </body>
 </html>
