ed2c4e179d36bfd316a09760f04975d61304f9fd
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-09-15 22:56:39 -0700

    Add new file to the repository.

---
+++ updating_variables.php
@@ -0,0 +1,19 @@
+<?php 
+$name  = 'Guest';
+$name  = 'Narendra';
+$name  = 'Chowdary';
+$price = 5;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Updating Variables</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2>Welcome <?php echo $name; ?></h2>
+    <p>The cost of your candy is 
+       $<?php echo $price; ?> per pack.</p>
+  </body>
+</html>
