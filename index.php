af32e318e846d037eed41374f04d751f12366486
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-09-16 00:07:36 -0700

    Add file and changed file names

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php 
-$name      = 'Narendra';
-$favorites = ['Chocolate', 'Toffee', 'Fudge', 'kit kat',];
+$items    = 3;
+$cost     = 5;
+$subtotal = $cost * $items;
+$tax      = ($subtotal / 100) * 20;
+$total    = $subtotal + $tax;
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Echo Shorthand</title>
+    <title>Mathematical Operators</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>Welcome <?= $name ?></h2>
-    <p>Your favorite type of candy is:
-       <?= $favorites[3] ?>.</p>
+    <h2>Shopping Cart</h2>
+    <p>Items: <?= $items ?></p>
+    <p>Cost per pack: $<?= $cost ?></p>
+    <p>Subtotal: $<?= $subtotal ?></p>
+    <p>Tax: $<?= $tax ?></p>
+    <p>Total: $<?= $total ?></p>
   </body>
 </html>
